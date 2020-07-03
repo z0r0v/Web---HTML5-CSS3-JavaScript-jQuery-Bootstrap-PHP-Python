8c0980e529d4c4b50096d30d8b5e9b54d7ee6c7c
Your Name <3767082@mail.ru>
2020-07-03 14:33:07 +0300

    added test 18

---
+++ JS/test 18/index.js
@@ -0,0 +1,25 @@
+// 1 Какого типа переменная userName?
+var userName = "Jack";
+console.log("userName:",typeof userName);
+// 2 Какого типа переменная userLastName?
+var userLastName = "White";
+console.log("userLastName", typeof userLastName);
+// 3 Какого типа переменная userAge в 3 строке?
+var userAge = 43;
+console.log("userAge", typeof userAge);
+
+// 4 Какого типа переменная userAge в 4 строке?
+userAge = userLastName;
+console.log("userAge str 4",typeof userAge);
+// 5 Какого типа переменная isUserRockStar?
+var isUserRockStar = true;
+console.log("isUserRockStar", typeof isUserRockStar);
+// 6 Какое значение переменной isAlien в 6 строке?
+var isAlien;
+console.log("value isAlien str 6:", isAlien);
+// 7 Какое значение переменной isAlien в 7 строке?
+var isAlien = null;
+console.log("value isAlien str 7:", isAlien);
+
+
+
