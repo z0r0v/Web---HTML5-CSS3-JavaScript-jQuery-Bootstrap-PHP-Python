154b886092093878df4fd31c424b19935c19ae82
z0r0v <3767082@mail.ru>
2020-11-24 15:51:51 +0300

    uploaded teory

--- JS/85_arrayMethods/index.js
+++ JS/85_arrayMethods/index.js
@@ -1,2 +1,21 @@
-// 31
-// 123
+// push/pop
+//unshift/shift
+//indexOf
+//slice
+let names =["jon", "Jane", "Lack", "Jim"];
+console.log(names);
+let x = names.push("Jeen");
+x = names.push("Ivan");
+console.log(x);
+
+let y = names.pop();
+console.log(names);
+console.log(y);
+
+x = names.unshift("irene");
+console.log(names);
+console.log(x);
+
+y = names.shift();
+console.log(names);
+console.log(y);
